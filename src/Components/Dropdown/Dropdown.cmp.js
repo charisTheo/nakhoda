f82cf345c1519465efdf8dc45ac0cd04adf355b9
charisTheo <haristh@hotmail.com>
2019-01-02 20:10:32 +0200

    Add Avatar and dropdown component on top nav bar

---
+++ src/Components/Dropdown/Dropdown.cmp.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+    dropdown: {
+        maxHeight: 'inherit',
+    },
+    button: {
+        color: 'rgb(254, 255, 255)',
+        textTransform: 'initial',
+        fontSize: '10px',
+        minHeight: 'initial',
+        maxHeight: '30px',
+        padding: '2px 10px',
+    },
+    list: {
+        margin: 0,
+        listStyle: 'none',
+        textAlign: 'left',
+        padding: '6px 16px',
+        position: 'absolute',
+    },
+    listItem: {
+        maxHeight: '30px',
+        fontSize: '10px',
+        marginTop: '5px', 
+    },
+    icon: {
+        display: 'inline-block',
+        maxHeight: '30px',
+        fontSize: '12px',
+        color: 'rgb(254, 255, 255)',
+    },
+};
+
+export default class Dropdown extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listOpen: false,
+            selected: props.list[0],
+        }
+    }
+    
+    handleClickOutside() {
+        this.setState({
+            listOpen: false
+        });
+    }
+    
+    toggleList() {
+        this.setState(prevState => ({
+          listOpen: !prevState.listOpen
+        }));
+    }
+
+    toggleItem = (event) => {
+        this.setState({
+            selected: event.target.text,
+            listOpen: false
+        });
+    };
+
+    render() {
+        const{list} = this.props;
+
+        return (
+          <div style={styles.dropdown}>
+            <Button style={styles.button} onClick={() => this.toggleList()}>
+                <div style={styles.selected} className='theme-grey-text'>{this.state.selected}</div>
+                {this.state.listOpen
+                    ? <i style={styles.icon} className="material-icons">keyboard_arrow_up</i>
+                    : <i style={styles.icon} className="material-icons">keyboard_arrow_down</i>
+                }
+            </Button>
+            {this.state.listOpen && <ul style={styles.list} className='theme-grey-darker-background'>
+                {list.map((item, index) => (
+                    <li style={styles.listItem} key={index} className='theme-grey-text'>
+                        <a onClick={this.toggleItem}>
+                            {item}
+                        </a>
+                    </li>
+                ))}
+            </ul>}
+          </div>
+        )
+    }
+}
--- src/Components/Nav/NavBar.cmp.js
+++ src/Components/Nav/NavBar.cmp.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 
-// import './NavBar.cmp.css';
+import Dropdown from '../Dropdown/Dropdown.cmp';
+
 
 const styles = {
   header: {
@@ -17,17 +17,27 @@ const styles = {
   toolbar: {
     minHeight: 'initial',
     maxHeight: '30px',
+    height: '30px',
   },
-  link: {
+  button: {
     color: 'rgb(254, 255, 255)',
     textTransform: 'initial',
     fontSize: '12px',
+    minHeight: 'initial',
+    maxHeight: '30px',
+    padding: '2px 10px',
   },
   selected: {
     color: 'rgb(254, 255, 255)',
   },
   dropdown: {
     fontSize: '12px',
+  },
+  avatar: {
+    width: '18px',
+    height: '18px',
+    fontSize: '12px',
+    backgroundColor: 'rgb(65, 66, 67)',
   }
 };
 
@@ -63,34 +73,36 @@ export default class NavBar extends Component {
       <div className="NavBar">
         <AppBar style={styles.header} position="static">
           <Toolbar style={styles.toolbar}>
-            <Button onClick={() => {this.isActive(0)}} className={this.state.index === 0 ? 'theme-yellow' : ''} style={styles.link} href="#">
-              Iste Natus
-            </Button>
-            <Button onClick={() => {this.isActive(1)}} className={this.state.index === 1 ? 'theme-yellow' : ''} style={styles.link} href="#">
-              Explicabo
-            </Button>
-            <Button onClick={() => {this.isActive(2)}} className={this.state.index === 2 ? 'theme-yellow' : ''} style={styles.link} href="#">
-              Omnis
-            </Button>
-            <Button onClick={() => {this.isActive(3)}} className={this.state.index === 3 ? 'theme-yellow' : ''} style={styles.link} href="#">
-              ipsa quae
-            </Button>
-            <Button onClick={() => {this.isActive(4)}} className={this.state.index === 4 ? 'theme-yellow' : ''} style={styles.link} href="#">
-              Perspiciatis Unde
-            </Button>
-            
-            <FormControl>
-              <Select
-                value={this.state.value}
-                onChange={this.handleChange}
-                style={styles.dropdown}
-              >
-                <MenuItem value="Veris Veritatis">Veris Veritatis</MenuItem>
-                <MenuItem value='Veris'>Veris</MenuItem>
-                <MenuItem value='Veritatis'>Veritatis</MenuItem>
-              </Select>
-            </FormControl>
+            <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+            >
+              <Grid item>
+                <Button onClick={() => {this.isActive(0)}} className={this.state.index === 0 ? 'theme-yellow-text' : ''} style={styles.button} href="#">
+                  Iste Natus
+                </Button>
+                <Button onClick={() => {this.isActive(1)}} className={this.state.index === 1 ? 'theme-yellow-text' : ''} style={styles.button} href="#">
+                  Explicabo
+                </Button>
+                <Button onClick={() => {this.isActive(2)}} className={this.state.index === 2 ? 'theme-yellow-text' : ''} style={styles.button} href="#">
+                  Omnis
+                </Button>
+                <Button onClick={() => {this.isActive(3)}} className={this.state.index === 3 ? 'theme-yellow-text' : ''} style={styles.button} href="#">
+                  ipsa quae
+                </Button>
+                <Button onClick={() => {this.isActive(4)}} className={this.state.index === 4 ? 'theme-yellow-text' : ''} style={styles.button} href="#">
+                  Perspiciatis Unde
+                </Button>
+              </Grid>
+              
+              <Dropdown
+                list={['Veris Veritatis', 'Veris', 'Veritatis']}
+                />
 
+              <Avatar className='theme-grey-background' style={styles.avatar}>Z</Avatar>
+            </Grid>
           </Toolbar>
         </AppBar>
       </div>
